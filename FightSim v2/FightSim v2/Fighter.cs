c8a1d5f762d9eaa10a08b4d47f96d7896e6e2338
SvanteKrusberg <svante.krusberg@elev.ga.ntig.se>
2019-10-10 14:10:24 +0200

    Kopia av V1
    Min v1 blev knasig då jag råkade förstöra allt genom att flytta på filer...
    Det blir att jobba i det här repositoryn nu :)

---
+++ FightSim v2/FightSim v2/Fighter.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FightSim_v1
+{
+    class Fighter
+    {
+        //Nödvändiga variablar
+        private int hp = 100;
+        public string name;
+        //En random generator för att slumpa tal
+        Random generator = new Random();
+
+        //En metod som slumpar ett attack värde mellan 1 och 20, sedan returneras detta värde som en int
+        public int Attack()
+        {
+            int dmg = generator.Next(1, 21);
+            return dmg;
+        }
+
+        //En metod som skadar Fightern, denna tar emot en int som parameter
+        public void Hurt(int amount)
+        {
+            hp -= amount;
+
+        }
+
+        //En metod som håller koll på om Fightern är vid liv eller inte, returnerar true/false
+        public bool IsAlive()
+        {
+            if (hp > 0)
+            {
+                return true;
+
+            }
+            else
+            {
+                return false;
+
+            }
+
+        }
+
+        //Returnerar värdet på HP så den privata int:en kan visas i main
+        public int GetHp()
+        {
+            return hp;
+
+        }
+
+    }
+}
--- FightSim v2/FightSim v2/Program.cs
+++ FightSim v2/FightSim v2/Program.cs
@@ -3,13 +3,72 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
-namespace FightSim_v2
+namespace FightSim_v1
 {
     class Program
     {
         static void Main(string[] args)
         {
+
+            //Skapar två fighters
+            Fighter f1 = new Fighter();
+            Fighter f2 = new Fighter();
+
+            //Ger användaren chans att namnge båda fightersen
+            Console.WriteLine("What's the name for:");
+            Console.WriteLine("Fighter 1?");
+            f1.name = Console.ReadLine();
+
+            Console.WriteLine("Fighter 2?");
+            f2.name = Console.ReadLine();
+            Console.Clear();
+
+            //En loop som körs så länge båda Fightersen är vid liv
+            while (f1.IsAlive() == true && f2.IsAlive() == true)
+            {
+                //Fightersens hp visas för spelaren
+                Console.WriteLine(f1.name + "'s HP: " + f1.GetHp());
+                Console.WriteLine(f2.name + "'s HP: " + f2.GetHp());
+                Thread.Sleep(100);
+
+                //Fighter 2 tar damagen som Fighter 1 gör i sin attack metod
+                f2.Hurt(f1.Attack());
+                Console.WriteLine(f1.name + " deals " + f2.Attack() + " to " + f2.name + "!");
+                Thread.Sleep(100);
+
+                //Fighter 1 tar damagen som Fighter 2 gör i sin attack metod
+                f1.Hurt(f2.Attack());
+                Console.WriteLine(f2.name + " deals " + f1.Attack() + " to " + f1.name + "!");
+                Console.ReadLine();
+
+                Console.Clear();
+            }
+
+            //En if som körs om Fighter 1 är död
+            if (f1.IsAlive() == false)
+            {
+                Console.WriteLine(f2.name + " wins!");
+
+            }
+            //If som körs om Fighter 2 är död
+            else if (f2.IsAlive() == false)
+            {
+                Console.WriteLine(f1.name + " wins!");
+
+            }
+            //En if som körs om något annat okänt händer
+            else
+            {
+                Console.WriteLine("Random none existing bug encountered :)");
+
+            }
+
+            Console.ReadLine();
+
+
+
         }
     }
 }
